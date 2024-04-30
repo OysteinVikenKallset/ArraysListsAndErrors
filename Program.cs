4a4d5f0d83d7bed256a53df95d6fd554609edb44
Øystein Kallset <oysteinkallset@gmail.com>
2024-04-30 08:09:16 +0200

    Ferdig

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿int[] myNum = {10, 20, 30, 40, 20, 21};
+bool validInput = false;
+int userInput = 0;
+
+int findGreatesNumber(int[] array){
+    return array.Max();
+}
+
+Console.WriteLine(findGreatesNumber(myNum));
+Console.WriteLine("");
+
+Console.WriteLine("Skriv inn et tall");
+
+while(!validInput){
+
+ validInput = int.TryParse(Console.ReadLine(), out userInput);
+ if(!validInput){
+    Console.WriteLine("Du må skrive inn et heltall");
+ }
+}
+Console.WriteLine(userInput);
+
+try
+{
+    Console.WriteLine(10/userInput);
+}
+catch (System.DivideByZeroException)
+{
+    
+    Console.WriteLine("Du delte på 0, det er ikke mulig. ");
+}
+
+
+
+
+
+
